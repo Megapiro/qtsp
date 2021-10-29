91795047ebf69ccaa5907e22ca8e5f9081c549e3
Megapiro <framegapiro@gmail.com>
2021-10-29 18:55:18 +0200

    init setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='qtsp',
+    packages=find_packages(),
+    version='0.0.0',
+    description='',
+    author='',
+    license='',
+    install_requires=['numpy', 'pandas', 'dimod', 'dwave-neal', 'dwave-hybrid', 'pyqubo'],
+    setup_requires=[],
+)
